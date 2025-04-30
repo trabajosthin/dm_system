c370faaf08125c9e5b86b7f768661ea68ad9c6ed
Andrés Delgado <trabajosthin@gmail.com>
2025-04-30 09:17:12 -0500

    Correción base de datos

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
   define( 'DB_HOST', 'localhost' );
   define( 'DB_USER', 'root' );
   define( 'DB_PASS', '' );
-  define( 'DB_NAME', 'test' );
+  define( 'DB_NAME', 'dm_system' );
 
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
