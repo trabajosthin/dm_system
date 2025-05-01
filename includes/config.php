a4f6e7177c2c9a881fd804ce3bb42778820ed2f2
Andrés Delgado <trabajosthin@gmail.com>
2025-05-01 15:02:57 -0500

    Conexión de la base de datos

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 
-  define( 'DB_HOST', 'localhost' );
+  define( 'DB_HOST', '158.247.124.124' );
   define( 'DB_USER', 'root' );
-  define( 'DB_PASS', '' );
+  define( 'DB_PASS', '2IYB0{Xos4n"' );
   define( 'DB_NAME', 'dm_system' );
 
   error_reporting(E_ALL);
